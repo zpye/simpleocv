f2564208aedd76893fcf55d660800a9d131db108
yezupei <yezupei@qiyi.com>
2023-08-01 10:51:56 +0800

    build(xmake): add option "build_sample" in xmake

--- xmake.lua
+++ xmake.lua
@@ -1,13 +1,20 @@
 add_rules("mode.debug", "mode.release")
 
+option("build_sample")
+    set_default(false)
+    set_showmenu(true)
+option_end()
+
 target("simpleocv")
     set_kind("static")
     add_includedirs("include/", { public = true })
     add_files("src/*.cpp")
 
 -- sample
-target("simpleocv_basic")
-    set_kind("binary")
-    add_files("sample/basic.cpp")
-    add_defines("IMAGE_PATH=R\"($(curdir)/imgs)\"")
-    add_deps("simpleocv")
+if has_config("build_python") then
+    target("simpleocv_basic")
+        set_kind("binary")
+        add_files("sample/basic.cpp")
+        add_defines("IMAGE_PATH=R\"($(curdir)/imgs)\"")
+        add_deps("simpleocv")
+end
